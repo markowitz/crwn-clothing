fae4851450ef7506cb1d0d64d8978af60d0f43d7
Ikechukwu Nwakanma <icnwakanma@gmail.com>
2021-02-05 23:46:14 +0100

    update app with shop page, collection preview component, collection item component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import Homepage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import './App.css';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   )
 }
